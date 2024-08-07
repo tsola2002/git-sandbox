77189535748b5636e5209a1392799ab41c44bb7f
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2024-08-07 05:16:15 +0100

    Initial Commit

---
+++ 01-COMMAND-LINE-BASICS/basics.js
@@ -0,0 +1,18 @@
+// this will display your current directory
+pwd
+
+// this will clear your terminal screen
+clear 
+
+// this will change directorys
+cd
+
+// displays the contents of a file
+cat
+
+// this will list the files in a directory
+ls
+
+// this will delete a file
+rm about.py
+
---
+++ 02-LOCAL-GIT-REPOSITORY/config.js
@@ -0,0 +1,20 @@
+// this will configure your display name
+git config --global user.name "Omatsola Isaac Sobotie"
+
+// this will display your email address
+git config --global user.email "tsola2002@yahoo.co.uk"
+
+// this will list all configuration details
+git config --list
+
+git config --global core.editor "code --new-window --wait"
+
+
+git config --global color.ui true
+
+
+
+
+
+//this will view the git configuaration file
+cat .gitconfig
