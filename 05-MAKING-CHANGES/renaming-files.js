d41bf71c4d9d1db023e833a23c32f30597bd4fb8
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-02-18 07:06:31 +0100

    RENAME

--- 05-MAKING-CHANGES/renaming-files.js
+++ 05-MAKING-CHANGES/renaming-files.js
@@ -1,12 +1,14 @@
 // rename a file and check your git status
 git status
 
-
+// THE MANUAL WAY
 // do the 2 at the same time and it will count as a renamed file
 // git add the new file and delete the deleted file and check te status
 git add new_file.html
 git rm deleted_file.html
 
+
+// THE GIT WAY
 // the proper way of renaming a file
 // git will automatically move it to the staging index
 git mv old_file.html new-file.html
