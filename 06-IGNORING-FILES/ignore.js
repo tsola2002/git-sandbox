d9f4d624a88d500acb52e9ac3a4c3f7b2167f714
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-02-25 06:08:54 +0100

    ignoring files

--- 06-IGNORING-FILES/ignore.js
+++ 06-IGNORING-FILES/ignore.js
@@ -1,8 +1,18 @@
-// will ignire any file end in .php
+// will ignore any file end in .php
 *.php
 
+// dont ignore index.php
+!index.php
+
+// this ignore a range of numbers 0-9
+[0-9]
+
+// ignore everthing in videos folder
+assets/videos
 // this will comment you gitignore code
 #
 
-// this will ignore a tracked file
+git config --global core.excludesfile ~/.gitignore_global
+
+// this will ignore a tracked file by removing it from the staging index
 git rm --cached indexedDB.php
---
+++ 06-IGNORING-FILES/ignore.md
@@ -0,0 +1,11 @@
+files to ignore
+
+#compiled source code
+#packages and compressed files
+#logs and databases
+#operating system genrated files
+#user-uploaded assets(images, PDFS, videos)
+
+checkout
+https://help.github.com/articles/ignoring-files
+https://github.com/github/gitignore
