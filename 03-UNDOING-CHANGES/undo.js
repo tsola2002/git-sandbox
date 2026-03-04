a04f13e0bb29c590c8f40d2be202c21c0ffe9a93
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2026-03-04 06:43:16 +0100

    removing unracked files

--- 03-UNDOING-CHANGES/undo.js
+++ 03-UNDOING-CHANGES/undo.js
@@ -1,33 +1,56 @@
-// restore working directory changes without changing branches
+//01 restore working directory changes without changing branches
+// it does this by checking out the last version
+// double dash flag forces you to stay on the current branch
+git status
+git diff
 git checkout -- indexedDB.html
 
-// undo changes to our staging index(to assemble a commit)
-git reset HEAD indexedDB.html
+//02 undo changes to our staging index(to assemble a commit)
+// this will move changes from staging index back to working directory
+git reset HEAD index.html
 
-// undoing commits that changes has been made
+//03 undoing commits that changes has been made
+// amend flag will only work on the last commit
+// it can also be used to change the commit message specifically
+make changes
+gi add .
 git commit --ammend -m "items to bring"
 
-//retrieve old versions(this will place it in your staging index)
+//04 retrieve old versions(this will place it in your staging index)
+// this will checkout a certain version of the file
+git log
 git checkout 2907d1260 -- resources.html
-git diff--staged
+// this will check the differences agains the staging area
+git diff --staged
 
-// this will revert any chages made to the commit file 
+//05 this will revert any chages made to the commit file
+//anything deleted will be added and anything added will be deleted
+// this will pop-up a custom commit message
 git revert da38660bbcd206
 
-
-// check head pointer location 
-cat.git / HEAD
+//06
+// check head pointer location
+// this will reveal the latest commitid
+cat .git/HEAD
 cat .git/refs/heads/master
 
-//--soft does not change staging index or working directory
+//make a copy of all your commits
+//  --soft does not change staging index or working directory(does not get rid of your changes)
+// it moves the pointer and does nothing else
+// use the cat command to confirm the location of the head pointer 
 git reset --soft da33660bbcd 
 
 //mixed-- changes staging index to match the repository
+// it does not change your working directory
+// changes will not be deleted. they will jus move to working area
 git reset --mixed da33660bbcd
 
 // --hard  changes staging index ad working directory to match the repository
-//(changes made after them will be obliterated)
+//(any changes made after the spicified commit will be completely obliterated)
+// this will throw away everything that happened after a specific commitid
+git reset --hard da33660bbcd
 
-//removing untracked files(-n test run -f force-run)
+//10 removing untracked files(-n test run -f force-run)
+// add junk1 junk2 junk3 as examples
 git clean -n
 git clean -f
